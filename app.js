20083737600f720c7036b2e1c5bc0810d631fac6
Dj Hammond <59342048+Dj-glitch@users.noreply.github.com>
2021-11-15 15:22:40 -0600

    fixed clusterMap.js
    cluster map and bootstrap troubleshooting.

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-if (process.env.NODE_ENV !== 'production') {
-	require('dotenv').config()
-}
+// if (process.env.NODE_ENV !== 'production') {
+// 	require('dotenv').config()
+// }
+
+require('dotenv').config()
 
 const express = require('express')
 const path = require('path')
+// const mongodb = require('mongodb')
 const mongoose = require('mongoose')
 const ejsMate = require('ejs-mate')
 const session = require('express-session')
@@ -13,13 +16,20 @@ const methodOverride = require('method-override')
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
 const User = require('./models/user')
+const helmet = require('helmet')
+
+const mongoSanitize = require('express-mongo-sanitize')
 
 // Requiring routes from the routes folder.
 const userRoutes = require('./routes/users')
 const campgroundRoutes = require('./routes/campgrounds')
 const reviewRoutes = require('./routes/reviews')
+const { Store } = require('express-session')
+const MongoDBStore = require('connect-mongo')
+const dbUrl = 'mongodb://localhost:27017/yelp-camp' || process.env.DB_URL
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+// 'mongodb://localhost:27017/yelp-camp'
+mongoose.connect(dbUrl, {
 	useNewUrlParser: true,
 	// useCreateIndex: true, Not needed after Mongoose v5....
 	useUnifiedTopology: true,
@@ -41,19 +51,79 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(mongoSanitize())
+
+const secret = process.env.SECRET || 'thisshouldbeabettersecret!'
+
+const store = new MongoDBStore({
+	mongoUrl: dbUrl,
+	touchAfter: 24 * 60 * 60,
+	crypto: {
+		secret,
+	},
+})
+
+store.on('error', function (e) {
+	console.log('SESSION STORE ERROR', e)
+})
 
 const sessionConfig = {
-	secret: 'thisshouldbeabettersecret!',
+	store,
+	name: 'session',
+	secret,
 	resave: false,
 	saveUninitialized: true,
 	cookie: {
 		httpOnly: true,
+		// secure: true,
 		expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
 		maxAge: 1000 * 60 * 60 * 24 * 7,
 	},
 }
 app.use(session(sessionConfig))
 app.use(flash())
+app.use(helmet())
+
+const scriptSrcUrls = [
+	'https://stackpath.bootstrapcdn.com',
+	'https://api.tiles.mapbox.com',
+	'https://api.mapbox.com',
+	'https://kit.fontawesome.com',
+	'https://cdnjs.cloudflare.com',
+	'https://cdn.jsdelivr.net',
+	'https://code.jquery.com',
+]
+const styleSrcUrls = [
+	'https://kit-free.fontawesome.com',
+	'https://stackpath.bootstrapcdn.com',
+	'https://api.mapbox.com',
+	'https://api.tiles.mapbox.com',
+	'https://fonts.googleapis.com',
+	'https://use.fontawesome.com',
+]
+const connectSrcUrls = ['https://api.mapbox.com', 'https://*.tiles.mapbox.com', 'https://events.mapbox.com']
+const fontSrcUrls = []
+app.use(
+	helmet.contentSecurityPolicy({
+		directives: {
+			defaultSrc: [],
+			connectSrc: ["'self'", ...connectSrcUrls],
+			scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+			styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+			workerSrc: ["'self'", 'blob:'],
+			childSrc: ['blob:'],
+			objectSrc: [],
+			imgSrc: [
+				"'self'",
+				'blob:',
+				'data:',
+				'https://res.cloudinary.com/drgt0t014', //SHOULD MATCH YOUR CLOUDINARY ACCOUNT!
+				'https://images.unsplash.com',
+			],
+			fontSrc: ["'self'", ...fontSrcUrls],
+		},
+	})
+)
 
 app.use(passport.initialize())
 app.use(passport.session())
@@ -64,6 +134,7 @@ passport.deserializeUser(User.deserializeUser())
 
 // connect-flash middleware
 app.use((req, res, next) => {
+	// console.log(req.query)
 	res.locals.currentUser = req.user
 	res.locals.success = req.flash('success')
 	res.locals.error = req.flash('error')
